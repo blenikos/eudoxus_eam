6ce3f68050e10640dce2f185b72b41387e687bd2
blenikos <blenikos@gmail.com>
2014-07-20 18:30:32 +0300

    New Tests
    *Added new test for user utils

--- database/test.php
+++ database/test.php
@@ -1,20 +1,53 @@
 <?php
 
-//include 'utils/db_link.php';
-include 'utils/db_utils.php';
+include 'utils/db_link.php';
+include 'utils/user_utils.php';
 
- $Linker = new DBLink();          
- session_start();
- $_SESSION['link']=$Linker;
-        
- $success = $Linker->DBLinking();
+Global $Linker;
+
+$Linker= new DBLink();                 
+$success = $Linker->DBLinking();
 if($success == false) printf("CONNECTION ERROR!!! NO DATABASE AVAILABLE!!!");
 else printf("CONNECTION SUCCESSFULL!!!");
 
-$USER = new Student(1);
+$USER = new User();
 
+//$USER->register("blenikos","abcd17","Students","std179@di.gr");
+
+$USER->login("blenikos","pass");
+
+echo($USER->get_user("Username"));
+echo($USER->get_user("Password"));
+echo($USER->get_user("email"));
+echo("      ");
+echo($USER->get_student_info("Name"));
+echo($USER->get_student_info("Surname"));
+echo($USER->get_student_info("Department"));
+
+
+$USER->update_profile("std10179@di.uoa.gr","pass");
+
+echo($USER->get_user("Username"));
+echo($USER->get_user("Password"));
+echo($USER->get_user("email"));
+echo("      ");
+echo($USER->get_student_info("Name"));
+echo($USER->get_student_info("Surname"));
+echo($USER->get_student_info("Department"));
+
+$USER->update_student(111521000179,"Nikos","Mpletsis","6970478400",8,"Tmhma Plhroforikhs kai Thlepikoinwniwn");
+
+echo($USER->get_user("Username"));
+echo($USER->get_user("Password"));
+echo($USER->get_user("email"));
+echo("      ");
+echo($USER->get_student_info("Name"));
+echo($USER->get_student_info("Surname"));
+echo($USER->get_student_info("Department"));
+
+echo("\n");
 printf("USER CREATION SUCCESSFULL!!!");
 
-printf($USER->get_name());
+//printf($USER->get_user_type());
 
 ?>
